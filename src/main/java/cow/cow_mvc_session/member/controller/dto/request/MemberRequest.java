9b6e376b3a6a034098bb6ae78471b55a5800d10c
나경호 <gangho324@gmail.com>
2024-05-09 21:30:57 +0900

    feat: init setting

--- src/main/java/cow/cow_mvc_session/member/controller/dto/request/MemberRequest.java
+++ src/main/java/cow/cow_mvc_session/member/controller/dto/request/MemberRequest.java
@@ -3,24 +3,14 @@ package cow.cow_mvc_session.member.controller.dto.request;
 import cow.cow_mvc_session.member.entity.Member;
 import lombok.Getter;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
 @Getter
-@AllArgsConstructor
 public class MemberRequest {
-	Long id;
-	final String name;
-
-	@Builder
-	private MemberRequest(final String name) {
-		this.name = name;
-	}
+	private String name;
 
 	public static Member toEntity(String name) {
-		return Member.of(name);
+		return Member.builder()
+			.name(name)
+			.build();
 	}
 }
 
